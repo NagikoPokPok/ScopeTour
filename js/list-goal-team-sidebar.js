234bef8d3a4de25997b385ac574d8e7a8f626f6a
Trần Quang Hiếu <tranquanghieunvl.2018@gmail.com>
2025-02-15 21:56:01 +0700

    list goal - team - sidebar

---
+++ js/list-goal-team-sidebar.js
@@ -0,0 +1,37 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+  sidebar.classList.toggle('close')
+  toggleButton.classList.toggle('rotate')
+
+  closeAllSubMenus()
+}
+
+function toggleSubMenu(button){
+
+  // B1: Khi click vào một menu ( <=> menu đó chưa có class show)
+  // B2: Đóng tất cả menu lại
+  // B3: Gán class show vào menu mới click đó
+  // B4: Sau khi gán class thì sub menu sẽ được mở
+  if(!button.nextElementSibling.classList.contains('show')){
+    closeAllSubMenus()
+  }
+
+  button.nextElementSibling.classList.toggle('show')
+  button.classList.toggle('rotate')
+
+  if(sidebar.classList.contains('close')){
+    // Đọc cách sử dụng toggle
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+  }
+}
+
+// Khi đóng sub menu => remove show + rotate icon
+function closeAllSubMenus(){
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show')
+    ul.previousElementSibling.classList.remove('rotate')
+  })
+}
